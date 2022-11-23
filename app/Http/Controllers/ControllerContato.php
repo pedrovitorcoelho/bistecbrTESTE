1d5ee6b860b3a19b4d8ac7bee73e28564c6fd557
Pedro Vitor <pedrovitorcoelho@outlook.com>
2022-11-23 10:59:04 -0300

    formulario funcionando

--- app/Http/Controllers/ControllerContato.php
+++ app/Http/Controllers/ControllerContato.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use app\Mail\ContatoEmail;
+use App\Mail\ContatoEmail;
 
 class ControllerContato extends Controller
 {
